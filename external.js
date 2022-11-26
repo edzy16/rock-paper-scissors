719f8faa0707adc9b5439fe7a27fe5ba80e3b0d9
edzy16 <edwinsiby16@gmail.com>
2022-11-26 18:19:04 +0530

    developed UI

--- external.js
+++ external.js
@@ -1,56 +1,58 @@
-function getComputerChoice() {
+function getComputerChoice (){
     const choices = ['rock', 'paper', 'scissors'];
     const randomNumber = Math.floor(Math.random() *3);
     return choices[randomNumber];
 }
+var playerScore = 0;
+var computerScore = 0;
 
-function getPlayerchoice() {
-    const playerSelection = prompt("Choose rock, paper or scissors");
-    return playerSelection.toLowerCase();
-}
-
-function round(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    const computerSelection = getComputerChoice();
+    let resultFinal = document.querySelector("#result-text");
+    resultFinal.textContent = resultText;
+    
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return "You win! Rock beats scissors";
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return "You lose! Paper beats rock";
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return "You win! Paper beats rock";
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return "You lose! Scissors beats paper";
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return "You win! Scissors beats paper";
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return "You lose! Rock beats scissors";
-    }
-}
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = getPlayerchoice();
-        const computerSelection = getComputerChoice();
-        let result = round(playerSelection, computerSelection);
-        if (result.includes("win")) {
-            playerScore++;
-        } else if (result.includes("lose")) {
-            computerScore++;
-        }
-        console.log(result);
-        console.log("Player score: " + playerScore);
-        console.log("Computer score: " + computerScore);
-    }
-    if (playerScore > computerScore) {
-        console.log("You win the game!");
-    } else if (playerScore < computerScore) {
-        console.log("You lose the game!");
-    } else {
+        var resultText = `It's a tie! You both chose ${playerSelection}`;
         console.log("It's a tie!");
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        var resultText = `You win! ${playerSelection} beats ${computerSelection}`;
+        console.log("You win! Rock beats scissors");
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        var resultText = `You lose! ${computerSelection} beats ${playerSelection}`;
+        console.log("You lose! Paper beats rock");
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        var resultText = `You win! ${playerSelection} beats ${computerSelection}`;
+        console.log("You win! Paper beats rock");
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        var resultText = `You lose! ${computerSelection} beats ${playerSelection}`;
+        console.log("You lose! Scissors beats paper");
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        var resultText = `You win! ${playerSelection} beats ${computerSelection}`;
+        console.log("You win! Scissors beats paper");
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        var resultText = `You lose! ${computerSelection} beats ${playerSelection}`;
+        console.log("You lose! Rock beats scissors");
     }
+    if(resultText.includes("win")) {
+        playerScore++;
+    }
+    else if(resultText.includes("lose")) {
+        computerScore++;
+    }
+    let playerScoreText = document.querySelector("#player-score");
+    playerScoreText.textContent = "Player Score= "+playerScore;
+    let computerScoreText = document.querySelector("#computer-score");
+    computerScoreText.textContent = "Computer Score="+computerScore;
+    if(playerScore === 5) {
+        alert("You win!");
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else if(computerScore === 5) {
+        alert("You lose!");
+        playerScore = 0;
+        computerScore = 0;
+    }
+
 }
 
-game();
